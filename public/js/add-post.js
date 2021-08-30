d647b5d2738224fba08134d04ef7eff8f6f36f71
Andrew Keiser <webdev410@gmail.com>
2021-08-29 22:06:35 -0700

    fixed edit post

--- public/js/add-post.js
+++ public/js/add-post.js
@@ -1,7 +1,7 @@
 async function newFormHandler(event) {
 	event.preventDefault();
 
-	const title = document.querySelector('input[name="post-title"]').value;
+	const title = document.querySelector('input[name="title"]').value;
 	const post_content = document.querySelector('input[name="post_content"]').value;
 
 	const response = await fetch(`/api/posts`, {
