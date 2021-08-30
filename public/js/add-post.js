9f6cbda919562a781c107ed5cd1d85b9748d7e17
Andrew Keiser <webdev410@gmail.com>
2021-08-29 22:12:46 -0700

    fixed new post

--- public/js/add-post.js
+++ public/js/add-post.js
@@ -1,27 +1,28 @@
 async function newFormHandler(event) {
 	event.preventDefault();
 
+
 	const title = document.querySelector('input[name="title"]').value;
 	const post_content = document.querySelector('input[name="post_content"]').value;
 
 	const response = await fetch(`/api/posts`, {
-		method: "POST",
+		method: 'POST',
 		body: JSON.stringify({
 			title,
-			post_content,
+			post_content
 		}),
 		headers: {
-			"post_content-Type": "application/json",
-		},
+			'Content-Type': 'application/json'
+		}
+
 	});
 
 	if (response.ok) {
-		document.location.replace("/dashboard");
+		document.location.replace('/dashboard');
+
 	} else {
 		alert(response.statusText);
 	}
-}
+};
 
-document
-	.querySelector(".new-post-form")
-	.addEventListener("submit", newFormHandler);
+document.querySelector('#new-post-form').addEventListener('submit', newFormHandler);
